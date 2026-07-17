92cbe85df74d1abbf48d3ed37d6eea170971c71a
ahmed fathi <69588458+ahmedfci111@users.noreply.github.com>
2026-07-17 21:27:55 +0300

    Update userValidator.js

--- utils/validators/userValidator.js
+++ utils/validators/userValidator.js
@@ -3,7 +3,7 @@ const slugify = require("slugify");
 const User = require("../../models/userModel");
 const {
   validatorMiddleware,
-} = require("../../middlewares/validatorMiddleware");
+} = require('../../middleWares/validatorMiddleware')
 const bcrypt = require("bcryptjs");
 
 // ================= Create User =================
